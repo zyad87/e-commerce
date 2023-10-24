4884355e1db7fb082755cf8e7eae256b99128003
zyad87 <zyadt871@gmail.com>
2023-10-25 01:08:12 +0300

    git

---
+++ src/index.js
@@ -0,0 +1,11 @@
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './css/style.css'
+import '@fortawesome/fontawesome-free/js/all.min';
+
+
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item))
+
+
+console.log('zyad اهلا')
+console.log('zyad مرحبا')
