730071f1d4ec946d883497ce7cb05c402fb08f44
zyad87 <zyadt871@gmail.com>
2023-11-09 11:38:30 +0300

    صفحة عربة الشراء

--- src/index.js
+++ src/index.js
@@ -31,6 +31,50 @@ document.querySelectorAll(".color-option input[type='radio']").forEach(item =>{
 })
 
 
+// حساب سعر أجمالي المنتج
+
+// يتم تحديد جميع العناصر التي تحتوي على خاصية 'data-product-quantity'
+document.querySelectorAll('[data-product-quantity]').forEach(item => {
+    // يتم إضافة استماع لحدث 'change' على كل عنصر
+    item.addEventListener('change', () => {
+        // يتم الحصول على الكمية الجديدة المحددة من قبل المستخدم
+        const newQuantity = item.value;
+        
+        // يتم العثور على العنصر الأقرب الذي يحتوي على خاصية 'data-product-info'
+        const parent = item.closest('[data-product-info]');
+        
+        // يتم الحصول على سعر الوحدة للمنتج من خلال الخاصية 'data-product-price'
+        const pricePerUnit = parent.getAttribute('data-product-price');
+        
+        // يتم حساب السعر الإجمالي للمنتج باستخدام الكمية الجديدة
+        const totalPriceForProduct = newQuantity * pricePerUnit
+        
+        // يتم تحديث نص العنصر الذي يحمل الفئة '.total-price-for-product' بالقيمة الجديدة
+        parent.querySelector('.total-price-for-product').innerHTML = totalPriceForProduct + " ريال ";
+        
+        calculateTotalPrice()
+    })
+})
+
+document.querySelectorAll('[data-remove-from-card]').forEach(item => {
+    item.addEventListener('click', () => {
+        item.closest('[data-product-info]').remove()
+        calculateTotalPrice()
+    })
+})
+
+function calculateTotalPrice() {
+    let totalPriceForAllProduct = 0;
+    document.querySelectorAll('[data-product-info]').forEach(product => {
+        const pricePerUnite = product.getAttribute('data-product-price');
+        const quantity = product.querySelector('[data-product-quantity]').value
+        const totalPriceForProduct = pricePerUnite * quantity
+
+        totalPriceForAllProduct = totalPriceForAllProduct + totalPriceForProduct;
+    })
+    document.getElementById('total-price-for-all-product').innerHTML = totalPriceForAllProduct;
+}
+
 
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
